83c4c425ff25f48eba6a191643efcad7f9d838d4
jaulf <63316890+jaulf@users.noreply.github.com>
2020-07-01 21:33:14 +0100

    new commit

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
 import Link from "next/link";
+import Head from "next/head";
 
 export default function IndexPage() {
   return (
     <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/8.png" />
+      </Head>
       <h1>Hello Babes!</h1>
       Read{" "}
       <Link href="/posts/first-post">
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,10 +1,19 @@
 import Link from "next/link";
+import Head from "next/head";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
     <>
-      <h1>What's up </h1>
-      <Link href="../">Back home!</Link>
+      <Layout>
+        <Head>
+          <title>Create Next App | Posts</title>
+        </Head>
+        <h1>What's up </h1>
+        <Link href="../">
+          <a>Back home!</a>
+        </Link>
+      </Layout>
     </>
   );
 }

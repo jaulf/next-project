c837565db5aaea3bd80f1b6076395bdc3310815b
jaulf <63316890+jaulf@users.noreply.github.com>
2020-07-05 16:07:00 +0100

    new commit 5th

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,40 @@
+import Layout from "../components/layout.js";
+import utilStyles from "../styles/utills.module.css";
 import Link from "next/link";
-import Head from "next/head";
 
-export default function IndexPage() {
+export async function getStaticProps() {
+  const blog = await fetch("https://jsonplaceholder.typicode.com/users");
+  const blogPost = await blog.json();
+
+  return {
+    props: {
+      blogPost
+    }
+  };
+}
+
+export default function Home({ blogPost }) {
   return (
-    <div>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/8.png" />
-      </Head>
-      <h1>Hello Babes!</h1>
-      Read{" "}
-      <Link href="/posts/first-post">
-        <a>this post!</a>
-      </Link>
-    </div>
+    <Layout home>
+      <section className={utilStyles.headingMd}>
+        <p>I'm a web developer</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{" "}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {blogPost.map(({ id, username, name }) => (
+            <li className={utilStyles.listItem} key={username}>
+              <Link href="/posts/[id]" as={`/posts/${username}`}>
+                <a>{name}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
   );
 }
